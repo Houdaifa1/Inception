b0f82127f105f468b5ec18b08504c645577f5ced
hdrahm <houdaifadrahm@gmail.com>
2025-05-28 15:07:33 -0400

    may 28

--- srcs/wordpress/conf/entrypoint.sh
+++ srcs/wordpress/conf/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /var/html/www/index.php ]; then
     echo "Copying WordPress files to volume..."
-    cp -r /wordpress_src/* /var/html/www/
+    cp -r /wordpress_src/* /var/www/html/
 fi
 
 echo "Starting PHP-FPM..."
