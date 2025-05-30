7dd57a4ed207aab64937acd9dc239bba2f7bbd8b
hdrahm <houdaifadrahm@gmail.com>
2025-05-29 20:22:09 -0400

    may 30 no sleep

--- srcs/wordpress/conf/entrypoint.sh
+++ srcs/wordpress/conf/entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+set -e
 
-if [ ! -f /var/html/www/index.php ]; then
-    echo "Copying WordPress files to volume..."
+if [ ! -f /var/www/html/index.php ]; then
+    echo "[INFO] WordPress not found in volume, copying..."
     cp -r /wordpress_src/* /var/www/html/
 fi
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
-echo "Starting PHP-FPM..."
+sed -i \
+  -e "s|username_here|$DB_USER|" \
+  -e "s|password_here|$DB_PASSWORD|" \
+  -e "s|database_name_here|$DB_NAME|" \
+  -e "s|localhost|$DB_HOST|" \
+  /var/www/html/wp-config.php 
+
+echo "[INFO] Starting PHP-FPM..."
 exec "$@"
